3794369c22f4f3d786c5a95f1f81c9500bf32f94
yanben <yanbzybzy2@gmail.com>
2017-06-04 15:29:00 +0100

    Website V2, using Angular and Bootstrap for a mobile friendly site

---
+++ public/index.js
@@ -0,0 +1,10 @@
+/* Copyright (C) Wenbin Yan - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Wenbin Yan <yanbzybzy3@gmail.com>, May 2017
+ */
+
+var app = angular.module('GreenLeavesTCM', []);
+app.controller('IndexPageController', function($scope) {
+    $scope.name = "John Doe";
+});
